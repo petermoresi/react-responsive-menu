e9ce78063ba2446692232c16722a65aaee0b7aa7
Peter Moresi <peter.moresi@gmail.com>
2015-09-09 22:46:35 -0700

    Extract demo data

--- Demo.js
+++ Demo.js
@@ -1,11 +1,45 @@
 var Menu = require('./Menu');
 var React = require('react');
 
+var menuItems = [{
+  textContent: 'Home',
+  href: '#home'
+}, {
+  textContent: 'About ￬',
+  href: '#',
+  items: [{
+    textContent: 'Who We Area',
+    href: '#who-we-are'
+  }, {
+    textContent: 'Who We Area',
+    href: '#who-we-are'
+  }]
+}, {
+  textContent: 'Portfolio ￬',
+  href: '#',
+  items: [{
+    textContent: 'Photography',
+    href: '#photography'
+  }, {
+    textContent: 'Web & User Interface Design',
+    href: '#web-and-ui-design'
+  }, {
+    textContent: 'Illustration',
+    href: '#illustration'
+  }]
+}, {
+  textContent: 'News',
+  href: '#news'
+}, {
+  textContent: 'Contact',
+  href: '#contact'
+}];
+
 var Demo = React.createClass({
   render: function() {
     return (
       <div>
-        <Menu />
+        <Menu items={menuItems} />
       </div>
     );
   }
--- Menu.js
+++ Menu.js
@@ -13,39 +13,7 @@ var Menu = React.createClass({
     return {
       themeName: 'default',
       id: '0',
-      items: [{
-        textContent: 'Home',
-        href: '#home'
-      }, {
-        textContent: 'About ￬',
-        href: '#',
-        items: [{
-          textContent: 'Who We Area',
-          href: '#who-we-are'
-        }, {
-          textContent: 'Who We Area',
-          href: '#who-we-are'
-        }]
-      }, {
-        textContent: 'Portfolio ￬',
-        href: '#',
-        items: [{
-          textContent: 'Photography',
-          href: '#photography'
-        }, {
-          textContent: 'Web & User Interface Design',
-          href: '#web-and-ui-design'
-        }, {
-          textContent: 'Illustration',
-          href: '#illustration'
-        }]
-      }, {
-        textContent: 'News',
-        href: '#news'
-      }, {
-        textContent: 'Contact',
-        href: '#contact'
-      }]
+      items: []
     };
   },
   render: function() {
